80e1923aa1eec7c948d5ddb63669b17ea3ad4993
khanderao khot <kkhot@gmail.com>
2020-04-24 15:24:22 -0500

    added cart items in the card drop down component, styled those cart items. Added connect and mapStatToProps to get data from redux state in to the dropdown component and then used props system to pass the data into the cart items

--- src/components/cartDropdown/CartDropdown.js
+++ src/components/cartDropdown/CartDropdown.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CustomButton from '../customButton/CustomButton';
 import './CartDropdown.scss';
+import CartItem from '../cartItem/CartItem';
 
-const CartDropdown = ()=> {
+const CartDropdown = ({cartItems})=> {
+    
     return (
         <div className='cart-dropdown'>
             <div className='cart-items'>
-             CartDropdown
+                {cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                ))}
+                {/* cartItem is coming from redux mapStateToProps after using map and going
+                to the CartItem component as props.  */}
             </div>
             <CustomButton >GO TO CHECKOUT</CustomButton>
-            
         </div>
     );
 }
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: {cartItems} }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
