8a79bda057b484d01efb1b0f28f33a40451cf924
khanderao khot <kkhot@gmail.com>
2020-04-26 20:23:20 -0500

    Installed redux-persist package for session persistance. Wired up the redux-persist library by making changes to rootReducer file, store.js and index.js files. Created directoryReducer and directorySelectors. Migrated data from directory component state to directoryReducer in redux store. Restructured the directory component to wire up the changes.

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -1,57 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
 import './Directory.scss';
 import MenuItem from '../menuItem/MenuItem';
+import { selectDirectorySections} from '../../redux/directory/directorySelectors';
+import { createStructuredSelector } from 'reselect';
 
 
-class Directory extends React.Component {
+const Directory = ({sections}) => {
 
-    constructor () {
-        super();
-
-        this.state = {
-            sections : [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-              ]
-        }
-    }
-
-    render () {
         return (
             <div className='directory-menu'>
                 {
                     // this.state.sections.map( ({title, imageUrl, id, size, linkUrl}) =>(
-                      this.state.sections.map( ({id, ...otherSectionProps}) =>(
+                      sections.map( ({id, ...otherSectionProps}) =>(
                         <MenuItem key={id} {...otherSectionProps} />
                         // <MenuItem 
                         //     title= {title} // instead of writing sections.title and sections.id so on and so forth, this is destructured using {title, imageUrl, id, size} instead of (section). So now we dont have to write section.id or section.imageUrl so on and so forth. One does not have to repeat section all the time
@@ -62,10 +24,18 @@ class Directory extends React.Component {
                         // />
                     ))
                 }
-                
             </div>
         );
     }
-}
+    
 
-export default Directory;
+const mapStateToProps = createStructuredSelector ({
+  sections: selectDirectorySections
+})
+
+// otehr option is to write the code like this without createStructuredSelector
+// const mapStateToProps =(state)=> ({
+//   sections:selectDirectorySections(state)
+// })
+
+export default connect(mapStateToProps) (Directory);
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,18 +1,31 @@
 import {combineReducers} from 'redux';
 // import combineReducers function
 // this rootReducer file is like index.js file for reducers
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 // import all the reducers needs combining
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
+import directoryReducer from './directory/directoryReducer';
 
 // after all reducers are combined, export them as key value pairs in an object
 //export combine reducers function as default
 // keys assigned here in the combineReducer will become properties on state object.
 // ie state.keyName will give you the slice of data using mapStateToProps function
 // key name should be similar to reducer name which is similar to action creater name. 
-export default combineReducers({
+
+const persistConfig = {
+    key: 'root',
+    storage, 
+    whitelist: ['cart'] // here pass all the reducers to be persisted
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer, 
+    directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
 
